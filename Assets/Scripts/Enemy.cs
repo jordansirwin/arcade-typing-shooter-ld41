68509d11d2507487401153ca2caf9acdaf913fe6
jirwin <jordan.s.irwin@gmail.com>
2018-04-20 23:50:31 -0700

    Add background art

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -20,23 +20,15 @@ public class Enemy : MonoBehaviour {
 		transform.position = Vector2.MoveTowards(transform.position, _destination, Time.deltaTime * speed);
 	}
 
-	void Escape() {
-		GameManager.Instance.enemiesEscaped += 1;
-		Destroy(this.gameObject);
-	}
-
 	void OnTriggerEnter2D(Collider2D collider) {
-		Debug.Log("OnTriggerEnter2D!");
 		if(collider.gameObject.tag != "Bullet") return;
 
 		Die();
 	}
-	
-	void OnCollisionEnter2D(Collision2D collision) {
-		Debug.Log("OnCollisionEnter2D!");
-		if(collision.gameObject.tag != "Bullet") return;
 
-		Die();
+	void Escape() {
+		GameManager.Instance.enemiesEscaped += 1;
+		Destroy(this.gameObject);
 	}
 
 	void Die() {
